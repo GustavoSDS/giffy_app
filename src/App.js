bae667fa2a8b238caa8b29c8c1fb324a85294cb7
GustavoSDS <simondossantos18@gmail.com>
2023-07-05 21:46:33 -0300

    resolve bugs, settings of SEO and more

--- src/App.js
+++ src/App.js
@@ -10,52 +10,37 @@ import Logo from "./components/Logo";
 import { Error } from "./components/Messages/Error";
 
 import "./App.css";
-import { Helmet } from "react-helmet";
 
 function App() {
   return (
-    <>
-      <Helmet>
-        <title>GIFAY</title>
-        <meta name="description" content="GIFAY is a GIF search engine" />
-        <meta name="keywords" content="gif, search, engine, app, stickers" />
-        <meta name="author" content="Gustavo S. Dos Santos" />
-        <meta name="application-name" content="GIFAY" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#4285f4" />
-        <meta name="rating" content="General" />
-      </Helmet>
+    <StaticContext.Provider value={{
+      nameGif: 'default',
+      type: 'gifs',
+    }}>
 
-      <StaticContext.Provider value={{
-        nameGif: 'default',
-        type: 'gifs',
-      }}>
+      <Logo />
+      <Header />
 
-        <Logo />
-        <Header />
+      <GifContextProvider>
+        <Route
+          path="/"
+          component={Home}
+        />
+        <Route
+          path="/details/gif/:id"
+          component={Detail}
+        />
+        <Route
+          path="/404"
+          component={Error}
+        />
+        <Route
+          path="/:search/:keyword"
+          component={SearchResults}
+        />
 
-        <GifContextProvider>
-          <Route
-            path="/"
-            component={Home}
-          />
-          <Route
-            path="/details/gif/:id"
-            component={Detail}
-          />
-          <Route
-            path="/404"
-            component={Error}
-          />
-          <Route
-            path="/:search/:keyword"
-            component={SearchResults}
-          />
-
-        </GifContextProvider>
-      </StaticContext.Provider>
-    </>
+      </GifContextProvider>
+    </StaticContext.Provider>
   );
 }
 
